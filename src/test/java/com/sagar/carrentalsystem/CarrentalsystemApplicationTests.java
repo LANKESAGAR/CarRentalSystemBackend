3a442fc19de29e956bdd5797b144465e26c0ca21
sagar <sagarl.edu2022@gmail.com>
2025-09-10 19:13:27 -0400

    Initial Commit

---
+++ src/test/java/com/sagar/carrentalsystem/CarrentalsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sagar.carrentalsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarrentalsystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
